321de956f39684e0aa3c582d2352cb0af91a2333
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-13 14:50:30 -0400

    Game Stats 2.0

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -5,43 +5,39 @@
 
 using namespace std;
 
-// Game Stats
+// Game Stats 2.0
 int main()
 {
-    int score;
-    double distance;
-    char playAgain;
-    bool shieldsUp;
-
-    short lives, aliensKilled;
-
-    score = 0;
-    distance = 1200.76;
-    playAgain = 'y';
-    shieldsUp = true;
-    lives = 3;
-    aliensKilled = 10;
-
-    double engineTemp = 6572.89;
-
+    unsigned int score = 5000;
     cout << "\nscore: " << score << endl;
-    cout << "distance: " << distance << endl;
-    cout << "playAgain: " << playAgain << endl;
 
+    score = score + 100;
+    cout << "\nscore: " << score << endl;
+
+    score += 100;
+    cout << "\nscore: " << score << endl;
+    
+    int lives = 3;
+    ++lives;
     cout << "lives: " << lives << endl;
-    cout << "aliensKilled: " << aliensKilled << endl;
-    cout << "engineTemp: " << engineTemp << endl;
 
-    int fuel;
-    cout << "\nHow much fuel?\n";
-    cin >> fuel;
-    cout << "fuel: " << fuel << endl;
+    lives = 3;
+    lives++;
+    cout << "lives: " << lives << endl;
 
-    // Define a new name for an existing type; 
-    // often used to create shorter names for types with long names
-    typedef unsigned short int ushort;
-    ushort bonus = 10;
-    cout << "\nbonus: " << bonus << endl;
+    lives = 3;
+    int bonus = ++lives * 10;
+    cout << "lives, bonus = " << lives << ", " << bonus << endl;
+
+    lives = 3;
+    bonus = lives++ * 10;
+    cout << "lives, bonus = " << lives << ", " << bonus << endl;
+
+    // integer wrap around
+    score = 4294967295;
+    cout << "\nscore: " << score << endl;
+    ++score;
+    cout << "score: " << score << endl;
 
     return 0;
 }
