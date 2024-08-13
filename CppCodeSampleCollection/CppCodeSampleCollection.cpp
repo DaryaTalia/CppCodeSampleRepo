180456e28fabab8b33645d806183e84d52d9b868
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-13 12:26:42 -0400

    Expensive Calculator

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -5,8 +5,20 @@
 
 using namespace std;
 
+// Expensive Calculator
 int main()
 {
-    cout << "Game Over!" << endl;
+    cout << "7 + 3 = " << 7 + 3 << endl;
+    cout << "7 - 3 = " << 7 - 3 << endl;
+    cout << "7 * 3 = " << 7 * 3 << endl;
+
+    cout << "7 / 3 = " << 7 / 3 << endl;
+    cout << "7.0 / 3.0 = " << 7.0 / 3.0 << endl;
+
+    cout << "7 % 3 = " << 7 % 3 << endl;
+
+    cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+    cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
+
     return 0;
 }
