80b6e785c3189ee383001ad962af827fb251fbee
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-13 21:11:57 -0400

    Score Rater 3.0

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 
-// Score Rater 2.0
+// Score Rater 3.0
 int main()
 {    
     int score ;
@@ -15,10 +15,16 @@ int main()
     cin >> score;
     
     if (score >= 1000) {
-        cout << "You scored more 1000 or more. Impressive!\n";        
+        cout << "You scored 1000 or more. Impressive!\n";        
+    } 
+    else if (score >= 500) {
+        cout << "You scored 500 or more. Nice.\n";
+    }
+    else if (score >= 250) {
+        cout << "You scored 250 or more. Decent.\n";
     }
     else {
-        cout << "You scored less than 1000.\n";
+        cout << "You scored less than 250. Nothing to brag about.\n";
     }
 
     return 0;
