0760a8cdb80eb6c046bf406f5ce4bdb2acf93940
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-14 16:20:29 -0400

    String Tester

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -3,43 +3,53 @@
 
 #include "CppCodeSampleCollection.h"
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-// Counter
+// String Tester
 int main()
 {    
-    cout << "Counting forward:\n";
-    for (int i = 0; i < 10; ++i) {
-        cout << i << " ";
+    string word1 = "Game";
+    string word2 = "Over";
+    string word3(3, '!');
+
+    string phrase = word1 + " " + word2 + word3;
+    cout << "The phrase is: " << phrase << "\n\n";
+
+    cout << "The phrase has " << phrase.size() << " characters in it.\n\n";
+
+    cout << "The character at position 0 is: " << phrase[0] << "\n\n";
+
+    cout << "Changing the character at position 0.\n";
+    phrase[0] = 'L';
+    cout << "The phrase is now: " << phrase << "\n\n";
+
+    for (unsigned int i = 0; i < phrase.size(); ++i) {
+        cout << "Character at position " << i << " is: " << phrase[i] << endl;
     }
 
-    cout << "\n\nCounting backward:\n";
-    for (int i = 9; i >= 0; --i) {
-        cout << i << " ";
+    cout << "\nThe sequence 'Over' begins at location ";
+    cout << phrase.find("Over") << endl;
+
+    if (phrase.find("eggplant") == string::npos) {
+        cout << "'eggplant' is not the phrase. \n\n";
     }
 
-    cout << "\n\nCounting by fives:\n";
-    for (int i = 0; i <= 50; i += 5) {
-        cout << i << " ";
-    }
+    phrase.erase(4, 5);
+    cout << "the phrase is now: " << phrase << "\n\n";
 
-    cout << "\n\nCounting with null statements:\n";
-    int count = 0;
-    for ( ; count < 10; ) {
-        cout << count << " ";
-        ++count;
-    }
+    phrase.erase(4);
+    cout << "the phrase is now: " << phrase << "\n\n";
 
-    cout << "\n\nCounting with nested for loops:\n";
-    const int ROWS = 5;
-    const int COLUMNS = 3;
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLUMNS; ++j) {
-            cout << i << ", " << j << "  ";
-        }
+    phrase.erase();
+    cout << "the phrase is now: " << phrase << "\n\n";
+
+    if (phrase.empty()) {
+        cout << "\nThe phrase is no more.\n";
     }
 
     cout << endl;
