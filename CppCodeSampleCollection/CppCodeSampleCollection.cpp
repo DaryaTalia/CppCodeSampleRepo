d79e24d1b4478dc508608ec24114c9d6906db7f6
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-15 03:16:03 -0400

    World Jumble 2.0

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -12,7 +12,7 @@ using std::cin;
 using std::endl;
 using std::string;
 
-// World Jumble
+// World Jumble 2.0
 int main() { 
     // Word List
     enum fields { WORD, HINT, NUM_FIELDS };
@@ -32,43 +32,65 @@ int main() {
         {"independence", "Similar to Liberation"}
     };
 
+    // Scoring System
+    int points = 0;
+    const int hintDeduction = 3;
+    int game_order[NUM_WORDS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // Scramble Word Order
+    srand(static_cast<unsigned int>(time(0)));
+
+    for (int i = 0; i < NUM_WORDS; ++i) {
+        int index1 = (rand() % NUM_WORDS);
+        int index2 = (rand() % NUM_WORDS);
+        char temp = game_order[index1];
+        game_order[index1] = game_order[index2];
+        game_order[index2] = temp;
+    }
+
     // Enter the Game Loop
     char again = 'y';
 
+    // Welcome the Player
+    cout << "\t\tWelcome to Word Jumble!\n\n";
+    cout << "Unscramble the letters to make a word.\n";
+    cout << "You'll gain 1 point for each character in the word.\n";
+    cout << "You'll lose 1 point for each incorrect guess.\n";
+    cout << "Enter '-hint' or '-h' for a hint.\n";
+    cout << "If you ask for a '-hint', you'll lose " << hintDeduction << " points.\n";
+    cout << "Enter '-quit' r '-q' to quit the game.\n\n";
+
     while (again == 'y') {
-        // Random Word
-        srand(static_cast<unsigned int>(time(0)));
-        int choice = (rand() % NUM_WORDS);
-        string theWord = WORDS[choice][WORD];
-        string theHint = WORDS[choice][HINT];
+        for (int i = 0; i < NUM_WORDS; i++) {
+            cout << "\nRound " << i + 1 << "\n";
 
-        // Jumble Word
-        string jumble = theWord;
-        int length = jumble.size();
-        for (int i = 0; i < length; ++i) {
-            int index1 = (rand() % length);
-            int index2 = (rand() % length);
-            char temp = jumble[index1];
-            jumble[index1] = jumble[index2];
-            jumble[index2] = temp;
-        }
+            string theWord = WORDS[game_order[i]][WORD];
+            string theHint = WORDS[game_order[i]][HINT];
 
-        // Welcome the Player
-        cout << "\t\tWelcome to Word Jumble!\n\n";
-        cout << "Unscramble the letters to make a word.\n";
-        cout << "Enter 'hint' for a hint.\n";
-        cout << "Enter 'quit' to quit the game.\n\n";
-        cout << "The jumble is: " << jumble;
+            // Jumble Word
+            string jumble = theWord;
+            int length = jumble.size();
+            for (int i = 0; i < length; ++i) {
+                int index1 = (rand() % length);
+                int index2 = (rand() % length);
+                char temp = jumble[index1];
+                jumble[index1] = jumble[index2];
+                jumble[index2] = temp;
+            }
 
-        string guess;
-        cout << "\n\nYour guess: ";
-        cin >> guess;
-            while ((guess != theWord) && (guess != "quit")) {
-                if (guess == "hint") {
-                    cout << theHint;
+            cout << "The jumble is: " << jumble;
+
+            string guess;
+            cout << "\n\nYour guess: ";
+            cin >> guess;
+            while ((guess != theWord) && (guess != "-quit") && (guess != "-q")) {
+                if (guess == "-hint" || guess == "-h") {
+                    cout << theHint; 
+                    points -= hintDeduction;
                 }
                 else {
-                    cout << "Sorry, that's not it.";
+                    cout << "\nSorry, that's not it.";
+                    points--;
                 }
 
                 cout << "\n\nYour guess: ";
@@ -76,10 +98,17 @@ int main() {
                 cin >> guess;
             }
 
-        // Give Congrats
-        if (guess == theWord) {
-            cout << "\nThat's it! You guessed it!\n";
+            // Give Congrats
+            if (guess == theWord) {
+                cout << "\nThat's it! You guessed it!\n";
+            }
+
+            // Give Points
+            points += theWord.size();
         }
+
+        //Final Points
+        cout << "\n\nYou made " << points << " points!";
 
         // Play Again
         again = ' ';
