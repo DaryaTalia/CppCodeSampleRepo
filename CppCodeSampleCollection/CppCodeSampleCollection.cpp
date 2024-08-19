9e2a4518500e4f77f704b1476c0c9e60c9ddb660
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-19 17:11:41 -0400

    Yes or No

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -2,26 +2,47 @@
 //
 
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-// Instructions
+// Yes or No
 
-// function prototype (declaration)
-void instructions();
+char askYesNo();
+char askYesNo(string question);
 
 int main() { 
-    instructions();
+    char answer1 = askYesNo();
+    cout << "Thanks for answering: " << answer1 << "\n\n";
+
+    char answer2 = askYesNo("Do you wish to save your game?");
+    cout << "Thanks for answering: " << answer2 << "\n\n";
 
     cout << endl;
 
     return 0;
 }
 
-// function definition
-void instructions() {
-    cout << "Welcome to the most fun you've ever had with text!\n\n";
-    cout << "Here's how to play the game...\n";
+char askYesNo() {
+    char response;
+    do {
+        cout << "Please enter 'y' or 'n': ";
+        cin >> response;
+    } 
+    while (response != 'y' && response != 'n');
+
+    return response;
+}
+
+char askYesNo(string question) {
+    char response;
+    do {
+        cout << question << " (y/n): ";
+        cin >> response;
+    } while (response != 'y' && response != 'n');
+
+    return response;
 }
