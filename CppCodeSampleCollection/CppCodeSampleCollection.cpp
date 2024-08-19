5e9d81dfa562f7c5942ccd0813007726818bf64f
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-18 23:32:52 -0400

    Hero's Inventory 3.0

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -11,7 +11,7 @@ using std::endl;
 using std::string;
 using std::vector;
 
-// Hero's Inventory 2.1
+// Hero's Inventory 3.0
 
 int main() { 
     vector<string> inventory;
@@ -19,73 +19,40 @@ int main() {
     inventory.push_back("robe");
     inventory.push_back("amulet");
 
-    cout << "You have " << inventory.size() << " items.\n";
+    vector<string>::iterator myIterator;
+    vector<string>::const_iterator iter;
 
     cout << "\nYour items:\n";
-    for (unsigned int i = 0; i < inventory.size(); i++) {
-        cout << inventory[i] << endl;
+    for (iter = inventory.begin(); iter != inventory.end(); iter++) {
+        cout << *iter << endl;
     }
 
     cout << "\nYou trade your staff for a wand. \n";
-    inventory[0] = "wand";
+    myIterator = inventory.begin();
+    *myIterator = "wand";
     cout << "\nYour items:\n";
-    for (unsigned int i = 0; i < inventory.size(); i++) {
-        cout << inventory[i] << endl;
+    for (iter = inventory.begin(); iter != inventory.end(); iter++) {
+        cout << *iter << endl;
     }
 
-    cout << "\nThe item name '" << inventory[0] << "' has ";
-    cout << inventory[0].size() << " letters in it.\n";
+    cout << "\nThe item name '" << *myIterator << "' has ";
+    cout << (*myIterator).size() << " letters in it.\n";
 
-    cout << "\nYour amulet is destroyed in a fierce battle. \n";
-    inventory.pop_back();
+    cout << "\nThe item name '" << *myIterator << "' has ";
+    cout << myIterator->size() << " letters in it.\n";
+
+    cout << "\nYou recover a spell book from a slain enemy. \n";
+    inventory.insert(inventory.begin(), "spell book");
     cout << "\nYour items:\n";
-    for (unsigned int i = 0; i < inventory.size(); i++) {
-        cout << inventory[i] << endl;
+    for (iter = inventory.begin(); iter != inventory.end(); iter++) {
+        cout << *iter << endl;
     }
 
-    cout << "\nYou were robbed of all of your possesions by a thief. \n";
-    inventory.clear();
-    cout << "\nYour items:";
-    if (inventory.empty()) {
-        cout << "\nYou have nothing.\n";
-    }
-    else {
-        cout << "\nYou have at least one item.\n";
-    }
-
-    cout << endl;
-
-    vector<string> fridge(10, "nothing");
-    cout << "\nYour items:";
-    if (fridge.empty()) {
-        cout << "\nYour fridge is empty.\n";
-    }
-    else {
-        cout << "\nYou have at least one item in your fridge.\n";
-
-        cout << "\nYour items:\n";
-        for (unsigned int i = 0; i < fridge.size(); i++) {
-            cout << fridge[i] << endl;
-        }
-    }
-
-    vector<string> groceries(5);
-    groceries[0] = "Eggs";
-    groceries[1] ="Bacon";
-    groceries[2] = "Sausage";
-    groceries[3] = "Lettuce";
-    groceries[4] = "Tomatoes";
-
-    for (unsigned int i = 0; i < groceries.size(); i++) {
-       fridge[i] = groceries[i];
-    }
-    groceries.clear();
-
-    //vector<string> fridge(groceries);
-
+    cout << "\nYour wand is destroyed in a fierce battle. \n";
+    inventory.erase((inventory.begin() + 1));
     cout << "\nYour items:\n";
-    for (unsigned int i = 0; i < fridge.size(); i++) {
-        cout << fridge[i] << endl;
+    for (iter = inventory.begin(); iter != inventory.end(); iter++) {
+        cout << *iter << endl;
     }
 
     cout << endl;
