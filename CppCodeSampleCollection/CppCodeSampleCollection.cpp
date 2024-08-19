4b43431923f7dddd9eeb0a95dca62457ea2cd40a
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-19 00:56:24 -0400

    Hangman and Psuedocode

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -12,41 +12,59 @@ using std::cin;
 using std::endl;
 using std::vector;
 
-// STL Containers and Psuedocode
+// Hangman and Psuedocode
 
 int main() { 
     // Psuedocode
     
-    // If Snake is suspected by an enemy
-    //      The enemy should investigate
-    //      Snake should evade
-    // Otherwise
-    //      The enemy should continue their patrol
-    //      Snake can continue sneaking
-    // Snake can collect ammo pack
-    // Snake can change uniform
-    // Snake can leave the area
-
-
-    // Stepwise Refinement
-    
-    // Snake can collect ammo pack:
-
-    // Move Snake to ammo's position
-    // Add ammo to Snake's inventory
-    // Destroy ammo from game world (or add to hidden pool)
+    // Generate a vector list of words, <string>
+    // Generate a new seed of random numbers using current time
+    // Randomly select a word from the vector and save as the secret word
+    // 
+    // Initialize an empty vector list for letters used, <char>
+    // 
+    // Initialize a vector list of characters equal to the size of the random word where each <character> is '-' for the guessed word
+    // 
+    // Initialize a constant int called MAX_GUESSES and declare to 8
+    // Initialize an int called guesses and declare to 0
+    // 
+    // Declare correctGuess as a bool
+    // 
+    // Welcome the player
+    // Tell the player how many incorrect guesses they have left (MAX_GUESSS - guesses)
+    // 
+    // 
+    // While guesses is less than MAX_GUESSES or the guessed word is not equal to the secret word:
+    // 
+    //      Print letters already used (none at start)
+    //      Print the guessed word ('-'s at start)
+    // 
+    //      Ask the player to enter one letter as a guess that is not already in the list of guessed characters and is between a to z
+    //      Initialize correctGuess to false
+    // 
+    //      Search the entire secret word for the guessed character:
+    //          If the character is found:
+    //              Replace the matching index of the guessed word with the character
+    //              If correctGuess is false:
+    //                  Add the character to the end of the list of guessed characters
+    //                  Mark correctGuess true
+    // 
+    //      If correctGuess is equal to false, increment guesses by 1
+    //          Tell the player they guessed incorrectly
+    //      Otherwise
+    //          Tell the player they guessed correctly
+    // 
+    //      Tell the player how many incorrect guesses they have left (MAX_GUESSS - guesses)
+    // 
+    // 
+    // While loop ends when the secret word is fully found or when guesses is equal to or greater than max guesses
+    // 
+    // Tell the player what the secret word was
+    // 
+    // Congratulate the player if they won or 
+    // tell the player if they lost
+    // 
+    // 
 
     return 0;
 }
-
-// STL Containers
-
-// deque            sequential      double-ended queue
-// list             sequential      linear list
-// map              associative     key/value pairs, 1 key to 1 value
-// multimap         associative     key/value pairs, 1 key to 1+ values
-// priority_queue   adaptor         priority queue
-// queue            adaptor         queue
-// set              associative     each elemt is unique
-// stack            adaptor         stack
-// vector           sequential      dynamic array
