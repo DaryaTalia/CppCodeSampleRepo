676b89b0a62263406521bdec680d1d401b82401b
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-19 17:33:08 -0400

    Give Me a Number

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -2,44 +2,36 @@
 //
 
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-// Global Reach
+// Give Me a Number
 
-int glob = 10;
-
-void access_global();
-void hide_global();
-void change_global();
+int askNumber(int high, int low = 1);
 
 int main() { 
-    cout << "In main() glob is: " << glob << "\n\n";
-    access_global();
+    int number = askNumber(5);
+    cout << "Thanks for entering: " << number << "\n\n";
 
-    hide_global();
-    cout << "In main() glob is: " << glob << "\n\n";
-
-    change_global();
-    cout << "In main() glob is: " << glob << "\n\n";
+    number = askNumber(10, 5);
+    cout << "Thanks for entering: " << number << "\n\n";
 
     cout << endl;
 
     return 0;
 }
 
-void access_global() {
-    cout << "In access_global() glob is: " << glob << "\n\n";
-}
+int askNumber(int high, int low) {
+    int num;
+    do {
+        cout << "Please enter a number (" << low << " - " << high << "): ";
+        cin >> num;
+    } 
+    while (num > high || num < low);
 
-void hide_global() {
-    int glob = 0;
-    cout << "In hide_global() glob is: " << glob << "\n\n";
-}
-
-void change_global() {
-    glob = -10;
-    cout << "In change_global() glob is: " << glob << "\n\n";
+    return num;
 }
