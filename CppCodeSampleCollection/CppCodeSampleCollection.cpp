3612e6878319662af9508113fe0e8e63412c6bd7
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-19 19:26:30 -0400

    Give Me a Number 2.0

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -3,98 +3,27 @@
 
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <vector>
-#include <algorithm>
-#include <cctype>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-using std::vector;
 
-// Hangman 2.0
+// Give Me a Number 2.0
 
-char getGuess(vector<char> *lettersUsed);
-bool evaluateGuess(string secret, string* guessedWord, char guessedChar, vector<char> *lettersUsed);
-int score;
-const int AWARD = 10;
-const int DEDUCT = 5;
+int getNumber(string prompt = "What number am I thinking? ");
 
 int main() { 
     bool playAgain = true;
     char play;
-    vector<string> words = { "hamilton", "mathematics", "medieval", "game", "mug", "juice" };
-
-    const int MAX_GUESSES = 8;
-
-    int roundsPlayed = 1;
-    int highScore = 0;
 
     while (playAgain) {
-        score = 0;
-        int guesses = 0;
-        srand(static_cast<unsigned int>(time(0)));
-        string secretWord = words[(rand() % words.size())];
-        const int MAX_SCORE = secretWord.size() * 10;
-        vector<char> *lettersUsed = new vector<char>(MAX_GUESSES + secretWord.size());
-        string* guessedWord = new string(secretWord.size(), '-');
-        bool correctGuess;
-        vector<char>::const_iterator iter;
+        // App
+        int number = getNumber();
+        cout << number << endl << endl;
+        number = getNumber("Give me a number: ");
+        cout << number << endl << endl;
 
-
-        cout << "Welcome to Hangman.\n\n";
-        cout << "Correct guesses award " << AWARD << " points per letter.\n";
-        cout << "Incorrect guesses deduct " << DEDUCT << " points.\n";
-        cout << "Total possible points this round: " << MAX_SCORE << "\n\n";
-
-        while (guesses < MAX_GUESSES && *guessedWord != secretWord) {
-            cout << "You have " << (MAX_GUESSES - guesses) << " guesses left." << endl;
-
-            cout << "You have already guessed: ";
-            for (iter = (*lettersUsed).begin(); iter != (*lettersUsed).end(); iter++) {
-                cout << *iter << " ";
-            }
-
-            cout << endl << endl;
-
-            for (unsigned int i = 0; i < (*guessedWord).size(); i++) {
-                cout << (*guessedWord)[i];
-            }
-
-            cout << endl << endl;
-
-            char guess = getGuess(lettersUsed);
-
-            correctGuess = evaluateGuess(secretWord, guessedWord, guess, lettersUsed);
-
-            if (correctGuess) {
-                cout << "Your guess is right!\n";
-            }
-            else {
-                cout << "Your guess is wrong.\n";
-                score -= DEDUCT;
-                guesses++;
-            }
-
-            cout << endl;
-        }
-
-        cout << "The secret word was: " << secretWord << endl;
-
-        cout << endl;
-
-        if (secretWord == *guessedWord) {
-            cout << "Congratulations!\n";
-        }
-        else {
-            cout << "Better luck next time.\n";
-        }
-
-        cout << "\nYour final score is: " << score << "/" << MAX_SCORE << endl;
-
-        cout << endl;
 
         // Play Again?
         play = ' ';
@@ -104,17 +33,8 @@ int main() {
             cin >> play;
         }
 
-        if (score > highScore) {
-            highScore = score;
-        }
-
         if (play == 'n') {
             playAgain = false;
-            cout << "\nYour highest score is: " << highScore << endl;
-            cout << "\nYou played " << roundsPlayed << " rounds." << endl;
-        }
-        else {
-            roundsPlayed++;
         }
 
         cout << endl;
@@ -125,32 +45,12 @@ int main() {
     return 0;
 }
 
-char getGuess(vector<char> *lettersUsed) {
-    char guess = ' ';
-    while (guess < 'a' || guess > 'z') {
-        cout << "Guess a letter: ";
-        cin >> guess;
-        cout << endl;
-
-        if (find((*lettersUsed).begin(), (*lettersUsed).end(), guess) != (*lettersUsed).end()) {
-            guess = ' ';
-        }
-    }
-    return guess;
+int getNumber(string prompt) {
+    int num = 0;
+    cout << prompt;
+    cin >> num;
+    return num;
 }
 
-bool evaluateGuess(string secret, string* guessedWord, char guessedChar, vector<char> *lettersUsed) {
-    bool result = false;
-    for (unsigned int i = 0; i < secret.size(); i++) {
-        if (guessedChar == secret[i]) {
-            (*guessedWord)[i] = guessedChar;
-            score += AWARD;
-            result = true;
-        }
 
-        if (find((*lettersUsed).begin(), (*lettersUsed).end(), guessedChar) == (*lettersUsed).end()) {
-            (*lettersUsed).push_back(guessedChar);
-        }
-    }
-    return result;
-}
+
