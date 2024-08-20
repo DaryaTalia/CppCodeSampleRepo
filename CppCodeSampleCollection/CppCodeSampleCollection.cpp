6ac7c0b877b877f65cfae2a6da158abed35dfaf2
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-20 02:47:03 -0400

    Static Critter

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -7,13 +7,17 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-// Private Critter
+// Static Critter
 
 // For Classes, members are private by default
 class Critter {
 public:
+    static int s_Total;             //static member variable declaration, s_
+
     Critter(int hunger = 0);
-    int GetHunger() const;          // accessor member function allows indirect access
+    ~Critter();
+    static int GetTotal();          //static member function prototype
+    int GetHunger() const;
     void SetHunger(int hunger);
     void Greet();
 
@@ -22,21 +26,28 @@ private:
 
 };
 
+int Critter::s_Total = 0;           //static member variable initialization
+
 Critter::Critter(int hunger) : m_Hunger(hunger) {
     cout << "A new critter has been born!" << endl;
+    ++s_Total;
     Greet();
 }
 
-// constant member functions can't modify a data member of its class or call a non-constant member function of its class
-// const at the end of the function header
-// ask only for what you need!
-// constant member functions *can* alter static data members
-// if a data member is keyworded with "mutable", even a constant member function can modify it
+Critter::~Critter() {
+    cout << "Bye!" << endl;
+    s_Total--;
+}
+
+int Critter::GetTotal() {           //static member function definition
+    return s_Total;
+}
+
 int Critter::GetHunger() const {
     return m_Hunger;
 }
 
-void Critter::SetHunger(int hunger) {     // accessor member function allows controlled access
+void Critter::SetHunger(int hunger) {
     if (hunger < 0) {
         cout << "You can't set a critter's hunger to a negative number.\n\n";
     }
@@ -45,23 +56,35 @@ void Critter::SetHunger(int hunger) {     // accessor member function allows con
     }
 }
 
-// Member function definition
 void Critter::Greet() {
     cout << "Hi. I'm a critter. My hunger level is " << m_Hunger << ".\n\n";
 }
 
 int main() {
-    Critter crit(5);
-    //cout << crit.m_Hunger;        // illegal, m_Hunter is private
-    cout << "Calling GetHunger(): " << crit.GetHunger() << "\n\n";
+    cout << "The total number of critters is: ";
+    cout << Critter::GetTotal() << "\n\n";
+
+    Critter crit1(5), crit2, crit3;
+    cout << "Calling GetHunger(): " << crit1.GetHunger() << "\n\n";
 
     cout << "Calling SetHunger() with a -1.\n";
-    crit.SetHunger(-1);
+    crit1.SetHunger(-1);
 
     cout << "Calling SetHunger() with a 9.\n";
-    crit.SetHunger(9);
+    crit1.SetHunger(9);
 
-    cout << "Calling GetHunger(): " << crit.GetHunger() << "\n\n";
+    cout << "Calling GetHunger(): " << crit1.GetHunger() << "\n\n";
+
+    {
+        //Exists inside this scope only
+        Critter crit4;
+
+        cout << "The total number of critters is: ";
+        cout << Critter::GetTotal() << "\n\n";
+    }
+
+    cout << "The total number of critters is: ";
+    cout << Critter::GetTotal() << "\n\n";
 
     cout << endl;
 
