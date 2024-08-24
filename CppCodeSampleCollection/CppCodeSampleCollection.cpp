4613d55f89dc2fc2ae2f706965740ba205f9a597
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-24 01:33:51 -0400

    Heap Data Member

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -2,77 +2,115 @@
 //
 
 #include <iostream>
+#include <string>
 
 using std::cout;
 // using std::cin;
 using std::endl;
+using std::string;
+using std::ostream;
 
-// Heap
+// Heap Data Member
 
-int* intOnHeap();   // returns an int on the heap
-void leak1();       // creates a memory leak
-void leak2();       // creates another memory leak
+class Critter {
+public:
+    Critter(const string& name = " ", int age = 0);
+    ~Critter();
+    Critter(const Critter& c);
+    Critter& operator=(const Critter& c);
+    
+    void Greet() const;
+
+private:
+    // declare a data member as a pointer to point to a value on the heap
+    string* m_pName;
+    int m_Age;
+};
+
+Critter::Critter(const string& name, int age) {
+    cout << "Constructor called\n";
+    // allocate memory on the heap for a string object
+    // new returns a pointer for the object
+    m_pName = new string(name);
+    m_Age = age;
+}
+
+// use destructors (~) to clean up any pointers to the heap
+Critter::~Critter() {
+    cout << "Destructor called\n";
+    delete m_pName;
+}
+
+// copy constructor: same name as class, accepts a reference to an object of the class
+// make constant to protect og object from change
+// default copy construtor creates a member-wise copy
+// write custom cc when there are data members that point to a value in the heap
+// the default will produce a 'shallow copy' which will only copy the address, not create a
+// whole new object, this could create a dangling pointer if the memory is later freed.
+// copy constructor can perform a 'deep copy', new object, new chunk of memory
+Critter::Critter(const Critter& c) {
+    cout << "Copy Constructor called\n";
+    m_pName = new string(*(c.m_pName));
+    m_Age = c.m_Age;
+}
+
+// assignment operator member function
+// write custom ao when data members point to the heap
+Critter& Critter::operator=(const Critter& c) {
+    cout << "Overloaded Assignment Operator called\n";
+    if(this != &c) {
+        delete m_pName;
+        m_pName = new string(*(c.m_pName));
+        m_Age = c.m_Age;
+    }
+    return *this;
+}
+
+void Critter::Greet() const {
+    cout << "I'm " << *m_pName << " and I'm " << m_Age << " years old.\n";
+    cout << "&m_pName: " << &m_pName << endl;
+}
+
+void testDestructor();
+void testCopyConstructor(Critter aCopy);
+void testAssignmentOp();
 
 int main() {
-    // the new operator allocates memory on the heap and returns its address
-    // use new followed by the type of value to reserve space for
-    // pHeap points to allocated memory on the heap and not the stack
-    int* pHeap = new int;
-    *pHeap = 10;
-    cout << "*pHeap: " << *pHeap << "\n\n";
+    testDestructor();
+    cout << endl;
 
-    // memory on the heap can persist beyond the function in which it was allocated
-    int *pHeap2 = intOnHeap();
-    cout << "*pHeap2: " << *pHeap2 << "\n\n";
+    Critter crit("Poochie", 5);
+    crit.Greet();
+    testCopyConstructor(crit);
+    crit.Greet();
+    cout << endl;
 
-    cout << "Freeing memory pointed to by pHeap.\n\n";
-    // memory on the heap must be explicitly freed
-    // if 'new' is used, 'delete' must also be used
-    delete pHeap; 
-
-    cout << "Freeing memory pointed to by pHeap2.\n\n";
-    delete pHeap2;
-
-    // memory is now free on the heap but the local variables aren't directly affected
-    // pHeap and pHeap2 are now dangling pointers
-
-    // get rid of dangling pointers
-    // never attempt to derefernce a dangling pointer
-    // assign them with 0
-    pHeap = 0;
-    pHeap2 = 0;
+    testAssignmentOp();
 
     cout << endl;
 
     return 0;
 }
 
-// creates an object on the heap and returns a pointer or reference to it
-// returns a pointer to this chunk of memory thanks to dynamic memory
-int* intOnHeap() {
-    int* pTemp = new int(20);
-    return pTemp;
+void testDestructor() {
+    Critter toDestroy("Rover", 3);
+    toDestroy.Greet();
 }
 
-// memory leaks: allocating memory but losing any way to access it again or feee it
-// large leaks can cause a program to run out of memory and crash
-
-// memory lost until the programs ends
-// drip1 is a local variable and the only way to access this chunk of memory
-// local variable will stop existing after this scope region ends 
-// but the corresponding memory will still be reserved for int(30)
-// no way to free the allocated meory
-// ways to resolve: delete to free the memory, or return a copy of the pointer drip1
-// memory should later be freed elsewhere if a pointer is returned
-void leak1() {
-    int* drip1 = new int(30);
+void testCopyConstructor(Critter aCopy) {
+    aCopy.Greet();
 }
 
-void leak2() {
-    // one chunk of memory
-    int* drip2 = new int(50);
-    // another chunk of memory
-    drip2 = new int(100);   // int(50) is no longer accessible
-    delete drip2;           // only frees int(100)
-    // drip2 was a dangling pointer, but will stop existing at the end of this function
+void testAssignmentOp() {
+    Critter crit1("cri1", 7);
+    Critter crit2("cri2", 9);
+    crit1 = crit2;
+
+    crit1.Greet();
+    crit2.Greet();
+    cout << endl;
+
+    Critter crit3("crit3", 11);
+    crit3 = crit3;
+    crit3.Greet();
 }
