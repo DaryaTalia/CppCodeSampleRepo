c967dd14a1864e4ed7309ea6dad35576e646dddd
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-26 13:56:14 -0400

    Overriding Boss

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -10,58 +10,88 @@ using std::endl;
 //using std::string;
 using std::ostream;
 
-// Simple Boss 2.0
+// Overriding Boss
 
 class Enemy {
 public:
-    Enemy();
-    void Attack() const;
+    Enemy(int damage = 10);
+    Enemy& operator=(const Enemy& e) {};
+    void virtual Taunt() const;        // made virtual to be overridden
+    void virtual Attack() const;        // made virtual to be overridden
 
 protected:
     int m_Damage;
 };
 
-Enemy::Enemy() : m_Damage(10) {}
+Enemy::Enemy(int damage) : m_Damage(damage) {}
+
+Enemy& Enemy::operator=(const Enemy& e) {
+    return *this;
+}
+
+void Enemy::Taunt() const {
+    cout << "The enemy says he will fight you.\n";
+}
 
 void Enemy::Attack() const {
-    cout << "Attack inflicts " << m_Damage << " damage points!\n";
+    cout << "Attack! Inflicts " << m_Damage << " damage points!\n";
 }
 
 
 class Boss : public Enemy {
 public:
-    Boss();
-    void SpecialAttack() const;
-
-private:
-    int m_DamageMultiplier;
+    Boss(int damage = 30);
+    Boss& operator=(const Boss& b) {};
+    void virtual Taunt() const;         // optional use of virtual keyword
+    void virtual Attack() const;        // optional use of virtual keyword
 };
 
-Boss::Boss() : m_DamageMultiplier(3) {}
+Boss::Boss(int damage) : Enemy(damage) {} // call base class constructor with argument
 
-void Boss::SpecialAttack() const {
-    cout << "Special attack inflicts " << (m_DamageMultiplier * m_Damage);
-    cout << " damage points!\n";
+// Not inherited from base class
+Boss& Boss::operator=(const Boss& b) { // or add    : Enemy(b) to handle inherited data members
+    Enemy::operator=(b);    // handles the data members inherited from Enemy
+    return *this;
+}
+
+void Boss::Taunt() const {              // override base class member function
+    cout << "The boss says he will end your pitiful existence.\n";
+}
+
+void Boss::Attack() const {
+    Enemy::Attack();                    // call base class member function
+    cout << "And laughs heartily at you.\n";
 }
 
 
 int main() {
-    cout << "Creating an enemy.\n";
-    Enemy enemy1;
-    enemy1.Attack();
+    cout << "Enemy object:\n";
+    Enemy anEnemy;
+    anEnemy.Taunt();
+    anEnemy.Attack();
 
-    cout << "\nCreating a boss.\n";
-    Boss boss1;
-    boss1.Attack();
-    boss1.SpecialAttack();
+    cout << "\n\nBoss object:\n";
+    Boss aBoss;     // the Enemy constructor is called first, and then the Boss constructor
+    aBoss.Taunt();
+    aBoss.Attack();
 
     cout << endl;
 
     return 0;
 }
 
-// These base class member functions are not inherited by derived classes:
-// Constructors, Copy Constructors, Destructors, Overloaded Assignment Operators
+// Declare any base class member function to be overridden as virtual
+// Provides a way for overridden versions of the member function to work as expected
+// with pointers and references to objects.
 // 
-// protected members are only accessible in their own class and certain derived classes
+// the virtual keyword is only needed in the declaration, not the definition
+// 
+// once a member function has been declared as virtual, it's virtual in any dervied class.
+// the virtual keyword doesn not need to be used again, but can be useful to recognize
+// a function as being virtual.
+// 
+// when you override an overloaded base class member function, all other overloaded versions
+// are hideen. the only way to access other versions is to explicitly call the base class
+// member function. Good idea to override every version of the overloaded function 
+// if one is overloaded.
 // 
