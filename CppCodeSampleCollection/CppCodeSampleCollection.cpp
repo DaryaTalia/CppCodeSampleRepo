fb0438a7efb98753f37408c3271abb99414a3afc
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-26 16:52:22 -0400

    Abstract Creature

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -10,91 +10,42 @@ using std::endl;
 //using std::string;
 using std::ostream;
 
-// Polymorphc Bad Guy
+// Abstract Creature
 
-class Enemy {
+class Creature {    // abstract class due to at least 1 pure virtual member function
 public:
-    Enemy(int damage = 10);
-    // make virtual to overrude and clear memory in derived classes
-    virtual ~Enemy();
-    Enemy& operator=(const Enemy& e);
-    void virtual Taunt() const;        // made virtual to be overridden
-    void virtual Attack() const;        // made virtual to be overridden
+    Creature(int health = 100);
+    virtual void Greet() const = 0;     // pure virtual member function, fx implementation depends on derived classes
+    virtual void DisplayHealth() const;
 
 protected:
-    int* m_pDamage;
+    int m_Health;
 };
 
-Enemy::Enemy(int damage) {
-    m_pDamage = new int(damage);
-}
+Creature::Creature(int health) : m_Health(health) {}
 
-Enemy::~Enemy() {
-    cout << "In Enemy destructor, deleting m_pDamage.\n";
-    delete m_pDamage;
-    m_pDamage = 0;
-}
-
-Enemy& Enemy::operator=(const Enemy& e) {
-    return *this;
-}
-
-void Enemy::Taunt() const {
-    cout << "The enemy says he will fight you.\n";
-}
-
-void Enemy::Attack() const {
-    cout << "An enemy attacks and inflicts " << *m_pDamage << " damage points.\n";
+void Creature::DisplayHealth()  const {
+    cout << "Health: " << m_Health << endl;
 }
 
 
-class Boss : public Enemy {
+class Orc : public Creature {
 public:
-    Boss(int multiplier = 3);
-    virtual ~Boss();
-    Boss& operator=(const Boss& b);
-    void virtual Taunt() const;         // optional use of virtual keyword
-    void virtual Attack() const;        // optional use of virtual keyword
-
-protected:
-    int* m_pMultiplier;
+    Orc(int health = 20);
+    virtual void Greet() const;
 };
 
-Boss::Boss(int multiplier) {
-    m_pMultiplier = new int(multiplier);
-}
+Orc::Orc(int health) : Creature(health) {}
 
-Boss::~Boss() {
-    cout << "In Boss destructor, deleting m_pMultiplier.\n";
-    delete m_pMultiplier;
-    m_pMultiplier = 0;
-}
-
-// Not inherited from base class
-Boss& Boss::operator=(const Boss& b) { // or add    : Enemy(b) to handle inherited data members
-    Enemy::operator=(b);    // handles the data members inherited from Enemy
-    return *this;
-}
-
-void Boss::Taunt() const {              // override base class member function
-    cout << "The boss says he will end your pitiful existence.\n";
-}
-
-void Boss::Attack() const {
-    cout << "A boss attacks and inflicts " << (*m_pDamage) * (*m_pMultiplier) << " damage points.\n";
+void Orc::Greet() const {
+    cout << "The orc grunts hello.\n";
 }
 
 
 int main() {
-    cout << "Calling Attack() on Boss object through pointer to Enemy:\n\n";
-    // can interact with derived objects w/o exactly knowing their type
-    Enemy* pBadGuy = new Boss();
-    pBadGuy->Attack();
-
-    cout << "\n\nDeleting pointer to Enemy:\n";
-    // derived objects delete in reversed order
-    delete pBadGuy;
-    pBadGuy = 0;
+    Creature* pCreature = new Orc();
+    pCreature->Greet();
+    pCreature->DisplayHealth();
 
     cout << endl;
 
