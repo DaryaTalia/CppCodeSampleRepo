cfe01d50c8de0015e673b86fdc2e15ae9a16812e
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-08-26 16:40:48 -0400

    Polymorphic Bad Guy

--- CppCodeSampleCollection/CppCodeSampleCollection.cpp
+++ CppCodeSampleCollection/CppCodeSampleCollection.cpp
@@ -10,20 +10,30 @@ using std::endl;
 //using std::string;
 using std::ostream;
 
-// Overriding Boss
+// Polymorphc Bad Guy
 
 class Enemy {
 public:
     Enemy(int damage = 10);
-    Enemy& operator=(const Enemy& e) {};
+    // make virtual to overrude and clear memory in derived classes
+    virtual ~Enemy();
+    Enemy& operator=(const Enemy& e);
     void virtual Taunt() const;        // made virtual to be overridden
     void virtual Attack() const;        // made virtual to be overridden
 
 protected:
-    int m_Damage;
+    int* m_pDamage;
 };
 
-Enemy::Enemy(int damage) : m_Damage(damage) {}
+Enemy::Enemy(int damage) {
+    m_pDamage = new int(damage);
+}
+
+Enemy::~Enemy() {
+    cout << "In Enemy destructor, deleting m_pDamage.\n";
+    delete m_pDamage;
+    m_pDamage = 0;
+}
 
 Enemy& Enemy::operator=(const Enemy& e) {
     return *this;
@@ -34,19 +44,31 @@ void Enemy::Taunt() const {
 }
 
 void Enemy::Attack() const {
-    cout << "Attack! Inflicts " << m_Damage << " damage points!\n";
+    cout << "An enemy attacks and inflicts " << *m_pDamage << " damage points.\n";
 }
 
 
 class Boss : public Enemy {
 public:
-    Boss(int damage = 30);
-    Boss& operator=(const Boss& b) {};
+    Boss(int multiplier = 3);
+    virtual ~Boss();
+    Boss& operator=(const Boss& b);
     void virtual Taunt() const;         // optional use of virtual keyword
     void virtual Attack() const;        // optional use of virtual keyword
+
+protected:
+    int* m_pMultiplier;
 };
 
-Boss::Boss(int damage) : Enemy(damage) {} // call base class constructor with argument
+Boss::Boss(int multiplier) {
+    m_pMultiplier = new int(multiplier);
+}
+
+Boss::~Boss() {
+    cout << "In Boss destructor, deleting m_pMultiplier.\n";
+    delete m_pMultiplier;
+    m_pMultiplier = 0;
+}
 
 // Not inherited from base class
 Boss& Boss::operator=(const Boss& b) { // or add    : Enemy(b) to handle inherited data members
@@ -59,39 +81,22 @@ void Boss::Taunt() const {              // override base class member function
 }
 
 void Boss::Attack() const {
-    Enemy::Attack();                    // call base class member function
-    cout << "And laughs heartily at you.\n";
+    cout << "A boss attacks and inflicts " << (*m_pDamage) * (*m_pMultiplier) << " damage points.\n";
 }
 
 
 int main() {
-    cout << "Enemy object:\n";
-    Enemy anEnemy;
-    anEnemy.Taunt();
-    anEnemy.Attack();
+    cout << "Calling Attack() on Boss object through pointer to Enemy:\n\n";
+    // can interact with derived objects w/o exactly knowing their type
+    Enemy* pBadGuy = new Boss();
+    pBadGuy->Attack();
 
-    cout << "\n\nBoss object:\n";
-    Boss aBoss;     // the Enemy constructor is called first, and then the Boss constructor
-    aBoss.Taunt();
-    aBoss.Attack();
+    cout << "\n\nDeleting pointer to Enemy:\n";
+    // derived objects delete in reversed order
+    delete pBadGuy;
+    pBadGuy = 0;
 
     cout << endl;
 
     return 0;
 }
-
-// Declare any base class member function to be overridden as virtual
-// Provides a way for overridden versions of the member function to work as expected
-// with pointers and references to objects.
-// 
-// the virtual keyword is only needed in the declaration, not the definition
-// 
-// once a member function has been declared as virtual, it's virtual in any dervied class.
-// the virtual keyword doesn not need to be used again, but can be useful to recognize
-// a function as being virtual.
-// 
-// when you override an overloaded base class member function, all other overloaded versions
-// are hideen. the only way to access other versions is to explicitly call the base class
-// member function. Good idea to override every version of the overloaded function 
-// if one is overloaded.
-// 
